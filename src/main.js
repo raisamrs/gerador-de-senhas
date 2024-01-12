ea9910cb08a5d6a5ddb643504f2e8f6071b89b12
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2024-01-12 11:15:48 -0300

    mais um commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('Hello, World!');
+import { nanoid } from 'nanoid';
+import './style.css';
+
+const passwordBtnEl = document.querySelector('button');
+const diplayPasswordEl = document.querySelector('h2');
+
+passwordBtnEl.addEventListener('click', () => {
+  const randomPassword = nanoid();
+  diplayPasswordEl.innerHTML = randomPassword;
+});
